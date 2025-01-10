ed0bd9c4aadb8b6a3b60716d87314cc78dac28b1
kinflux digital <126244085+diggilly@users.noreply.github.com>
2025-01-10 20:51:41 +0300

    Create clientConfig.js

---
+++ config/clientConfig.js
@@ -0,0 +1,9 @@
+// clientConfig.js
+export const clientConfig = {
+    // Example configuration for the client, add or modify based on your setup
+    puppeteer: {
+      headless: true, // Set to true for headless mode
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    },
+  };
+  
